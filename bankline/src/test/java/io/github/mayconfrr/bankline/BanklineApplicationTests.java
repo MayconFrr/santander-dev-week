1864491ad44854f61e47c1f57d215b4941391371
Maycon Ferreira <32937928+MayconFrr@users.noreply.github.com>
2022-05-06 14:13:01 -0300

    Setup application and dependencies

---
+++ bankline/src/test/java/io/github/mayconfrr/bankline/BanklineApplicationTests.java
@@ -0,0 +1,12 @@
+package io.github.mayconfrr.bankline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
